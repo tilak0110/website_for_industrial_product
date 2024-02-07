991e8d517c0605e024503775fbe6e0de4679efa8
tilak0110 <154294421+tilak0110@users.noreply.github.com>
2024-02-07 08:46:45 +0530

    Initial commit

---
+++ industrial_products_.net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web_application_for_industrial_products.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
