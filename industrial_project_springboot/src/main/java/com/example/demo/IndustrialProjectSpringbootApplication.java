43e8ca2202935af2ee2e89b77e826b2a2ae97c5e
tilak0110 <154294421+tilak0110@users.noreply.github.com>
2024-02-07 09:03:44 +0530

    Initial commit

---
+++ industrial_project_springboot/src/main/java/com/example/demo/IndustrialProjectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialProjectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustrialProjectSpringbootApplication.class, args);
+	}
+
+}
